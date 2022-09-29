97c3cb6120eb8cf851a8cf1d6b9066904b1adb00
Sébastien GUERRI <13090870+sguerri@users.noreply.github.com>
2022-09-29 16:57:46 +0200

    Initial commit

---
+++ PGeometry.h
@@ -0,0 +1,12 @@
+#ifndef PGEOMETRY_H
+#define PGEOMETRY_H
+
+#include <QMainWindow>
+
+namespace PGeometry
+{
+    extern void init(QMainWindow* window, const QString& name = "");
+};
+
+
+#endif // PGEOMETRY_H
